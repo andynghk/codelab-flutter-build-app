190545341f36bd8614694008b86a4618ca4454c1
Andy Ng <andy.ng@betterpi.com>
2022-07-02 04:31:01 +0800

    Update for last exercise

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -17,9 +17,11 @@ void main() {
   print(square.area);
 
   final values = [1, 2, 3, 5, 10, 50];
-  for (var length in values) {
-    print(scream(length));
-  }
+  // for (var length in values) {
+  //   print(scream(length));
+  // }
+  values.map(scream).forEach(print);
+  values.skip(1).take(3).map(scream).forEach(print);
 
   runApp(const MyApp());
 }
